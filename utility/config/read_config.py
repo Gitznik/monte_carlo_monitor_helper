f12bded0654e4954c1c49be4d58d3dd2f8fac116
Gitznik <r.offner@gmx.de>
2021-07-06 17:42:52 +0200

    Rename snowflake to database

--- utility/config/read_config.py
+++ utility/config/read_config.py
@@ -5,9 +5,9 @@ import os
 class yamlConfig:
     def __init__(self):
         self.full_config = self.load_config()
-        self.full_snowflake_secrets = self.load_secrets()
+        self.full_database_secrets = self.load_secrets()
         self.extract_table_config()
-        self.extract_snowflake_config()
+        self.extract_database_config()
         self.extract_monitor_config()
 
     def load_config(self):
@@ -18,7 +18,7 @@ class yamlConfig:
 
     def load_secrets(self):
         filepath = os.path.join(
-            os.path.dirname(__file__), 'snowflake_secrets.yaml')
+            os.path.dirname(__file__), 'database_secrets.yaml')
         with open(filepath) as secrets:
             return(yaml.full_load(secrets))
 
@@ -26,20 +26,20 @@ class yamlConfig:
         table_config = self.full_config['table_config']
         self.table_age_limit = table_config['table_age_limit']
         schemas = tuple(table_config['schemas'])
-        if len(schemas) == 0:
+        if not schemas:
             raise ValueError("No schemas provided")
         elif len(schemas) == 1:
             self.snowflake_schemas = f"('{schemas[0]}')"
         else:
             self.snowflake_schemas = schemas
 
-    def extract_snowflake_config(self):
-        snowflake_secrets = self.full_snowflake_secrets['snowflake_config']
-        self.snowflake_account = snowflake_secrets['account']
-        self.snowflake_db = snowflake_secrets['database']
-        self.snowflake_user = snowflake_secrets['user_name']
-        self.snowflake_wh = snowflake_secrets['warehouse']
-        self.snowflake_role = snowflake_secrets['role']
+    def extract_database_config(self):
+        database_secrets = self.full_database_secrets['database_config']
+        self.snowflake_account = database_secrets['account']
+        self.snowflake_db = database_secrets['database']
+        self.snowflake_user = database_secrets['user_name']
+        self.snowflake_wh = database_secrets['warehouse']
+        self.snowflake_role = database_secrets['role']
     
     def extract_monitor_config(self):
         monitor_config = self.full_config['monitor_config']
